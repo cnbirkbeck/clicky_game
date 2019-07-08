8c0ada128516210d85066d35dda4d5cb6d633372
CBirkbeck <cnbirkbeck@gmail.com>
2019-07-07 19:25:17 -0700

    updating app.js added imports and shuffle character function

--- clicky_game/src/App.js
+++ clicky_game/src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import CharacterCard from "./components/CharacterCard";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
+import characters from "./character.json";
 import './App.css';
+
+//set up shuffle function
+function shuffleCharacters(array) {
+  for (let i = array.length -1; i > 0; i--){
+    let j = Math.floor(Math.random() * (i+1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 function App() {
   return (
