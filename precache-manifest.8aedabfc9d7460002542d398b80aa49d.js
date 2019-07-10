6699703e0c8caf27fbbdbdd0a4cd1cda0f022c59
CBirkbeck <cnbirkbeck@gmail.com>
2019-07-10 12:30:51 -0700

    Updates

---
+++ precache-manifest.8aedabfc9d7460002542d398b80aa49d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b331a97f424a60bf81607f2d50efae1",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "09f29c89b11df3874ed4",
+    "url": "/clicky-game/static/css/main.38fcd34e.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "09f29c89b11df3874ed4",
+    "url": "/clicky-game/static/js/main.7d8f8b27.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
