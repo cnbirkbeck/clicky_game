ec2f500efaac8d9d498c8456cd925a8806ff731e
CBirkbeck <cnbirkbeck@gmail.com>
2019-07-10 12:38:17 -0700

    updated readme, and bug fix in images

--- clicky_game/src/App.js
+++ clicky_game/src/App.js
@@ -1,106 +1,105 @@
-import React from 'react';
-import CharacterCard from "./components/CharacterCard";
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
 import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import Container from "./Container";
 import Row from "./Row";
 import Column from "./Column";
-import characters from "./character.json";
-import './App.css';
+import friends from "./friends.json";
+import "./App.css";
 
-//set up shuffle function
-function shuffleCharacters(array) {
-  for (let i = array.length -1; i > 0; i--){
-    let j = Math.floor(Math.random() * (i+1));
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 };
- 
-//create app component
+
 class App extends Component {
- //set this.state
- state = {
-   characters,
-   currentScore: 0,
-   topScore: 0, 
-   rightWrong: "", 
-   clicked: [],
- };
+  // Set this.state
+  state = {
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: [],
+  };
 
- handleClick = id => {
-   if (this.state.clicked.indexOf(id) === -1) {
-     this.handleIncrement();
-     this.setState({ clicked: this.state.clicked.concat(id) });
-   } else {
-     this.handleReset();
-   }
- };
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
 
- handleIncrement = () => {
-   const newScore = this.state.currentScore + 1;
-   this.setState({
-     currentScore: newScore,
-     rightWrong: ""
-   });
-   if (newScore >= this.state.topScore) {
-     this.setState({ topScore: newScore});
-   }
-   else if (newScore === 12) {
-     this.setState({ rightWrong: "You Win!" });
-   }
-   this.handleShuffle();
- };
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "You win!" });
+    }
+    this.handleShuffle();
+  };
 
- handleReset = () => {
-   this.setState({
-     currentScore: 0,
-     topScore: this.state.topScore,
-     rightWrong: "Avada Kedavra!", 
-     clicked: []
-   });
-   this.handleShuffle();
- };
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Avada Kedavra!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
 
- handleShuffle = () => {
-   let shuffleCharacters = shuffleCharacters (characters);
-   this. setState({characters: shuffleCharacters});
- };
+  handleShuffle = () => {
+    let shuffledFriends = shuffleFriends(friends);
+    this.setState({ friends: shuffledFriends });
+  };
 
- render() {
-   return (
-     <Wrapper>
-       <Nav
-       title= "Harry Potter Clicky Game"
-       score = {this.state.currentScore}
-       topScore = {this.state.topScore}
-       rightWrong = {this.state.rightWrong}
-       />
-       <Title>
-         Try to click on each character without hitting any duplicates!
-       </Title>
+  render() {
+    return (
+      <Wrapper>
+        <Nav
+          title="Harry Potter Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        />
 
-       <Container>
-         <Row>
-           {this.state.characters.map(friend => (
-             <Column size= "md-3" "sm-6">
-               <CharacterCard
-                  key={characters.id}
+        <Title>
+          Try to click on each character, without selecting any duplicates!
+        </Title>
+
+        <Container>
+          <Row>
+            {this.state.friends.map(friend => (
+              <Column size="md-3 sm-6">
+                <FriendCard
+                  key={friend.id}
                   handleClick={this.handleClick}
                   handleIncrement={this.handleIncrement}
                   handleReset={this.handleReset}
                   handleShuffle={this.handleShuffle}
-                  id={characters.id}
-                  image={characters.image}
+                  id={friend.id}
+                  image={friend.image}
                 />
-              </Column>    
-           ))}
-         </Row>
-       </Container>
-     </Wrapper>
-   );
- }
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
